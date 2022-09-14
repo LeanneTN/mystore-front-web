c335a3b2c271b44724ce1b98ca3db00816dc1b5e
LeanneTN <2321604632@qq.com>
2022-09-14 23:20:02 +0800

    style: style of global page and buttons

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -1,2 +1,5 @@
+require('./mystore.css');
 var t = require('../../utils/util.js')
 console.log(t.a)
+
+module.exports = t
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,5 +1,6 @@
 console.log("index: index.js")
 require('./index.css')
+require('../common/index.js');
 var t = require('../../utils/util.js')
 
 console.log(t.a);
